1c5d77fa17f1a290dba3b7aee0328fef553b2a70
NokTNL <noklamtsz@gmail.com>
2023-10-12 00:00:21 +0100

    Add 20 max tests

--- src/pairtest/lib/getNumOfTixAndPrice.js
+++ src/pairtest/lib/getNumOfTixAndPrice.js
@@ -5,12 +5,18 @@ const ticketPriceByType = {
   CHILD: 10,
 };
 
+export class TooManyTicketsException extends Error {}
+
 /**
  * @param {TicketTypeRequest[]} requests
  * @returns {{price: number; numOfTix: number}}
  */
 export default function getNumOfTixAndPrice(requests) {
   const numOfTix = requests.reduce((sum, req) => sum + req.getNoOfTickets(), 0);
+
+  if (numOfTix > 20) {
+    throw new TooManyTicketsException();
+  }
   const price = requests.reduce(
     (sum, req) =>
       sum + ticketPriceByType[req.getTicketType()] * req.getNoOfTickets(),
--- test/units/unit.test.js
+++ test/units/unit.test.js
@@ -1,48 +1,90 @@
-import { test, expect, describe } from "vitest";
+import { describe, expect, test } from "vitest";
 import TicketTypeRequest from "../../src/pairtest/lib/TicketTypeRequest";
-import getNumOfTixAndPrice from "../../src/pairtest/lib/getNumOfTixAndPrice";
+import getNumOfTixAndPrice, {
+  TooManyTicketsException,
+} from "../../src/pairtest/lib/getNumOfTixAndPrice";
 
-test("1 adult => 20 pounds, 1 ticket", () => {
-  const requests = [new TicketTypeRequest("ADULT", 1)];
-  expect(getNumOfTixAndPrice(requests)).toEqual({
-    price: 20,
-    numOfTix: 1,
+describe("Normal scenarios with >= 1 one adult", () => {
+  test("1 adult => 20 pounds, 1 ticket", () => {
+    const requests = [new TicketTypeRequest("ADULT", 1)];
+    expect(getNumOfTixAndPrice(requests)).toEqual({
+      price: 20,
+      numOfTix: 1,
+    });
+  });
+  test("2 adults => 40 pounds, 2 tickets", () => {
+    const requests = [new TicketTypeRequest("ADULT", 2)];
+    expect(getNumOfTixAndPrice(requests)).toEqual({
+      price: 40,
+      numOfTix: 2,
+    });
+  });
+  test("1 adult, 1 child => 30 pounds, 2 tickets", () => {
+    const requests = [
+      new TicketTypeRequest("CHILD", 1),
+      new TicketTypeRequest("ADULT", 1),
+    ];
+    expect(getNumOfTixAndPrice(requests)).toEqual({
+      price: 30,
+      numOfTix: 2,
+    });
+  });
+  test("2 adult, 3 child => 70 pounds, 5 tickets", () => {
+    const requests = [
+      new TicketTypeRequest("CHILD", 3),
+      new TicketTypeRequest("ADULT", 2),
+    ];
+    expect(getNumOfTixAndPrice(requests)).toEqual({
+      price: 70,
+      numOfTix: 5,
+    });
+  });
+  test("2 adult, 3 child => 70 pounds, 5 tickets", () => {
+    const requests = [
+      new TicketTypeRequest("CHILD", 3),
+      new TicketTypeRequest("ADULT", 2),
+    ];
+    expect(getNumOfTixAndPrice(requests)).toEqual({
+      price: 70,
+      numOfTix: 5,
+    });
   });
 });
-test("2 adults => 40 pounds, 2 tickets", () => {
-  const requests = [new TicketTypeRequest("ADULT", 2)];
-  expect(getNumOfTixAndPrice(requests)).toEqual({
-    price: 40,
-    numOfTix: 2,
+describe("Max 20 tickets allowed", () => {
+  // TODO: test Infants
+  test("21 adults => throws TooManyTicketsException", () => {
+    const requests = [new TicketTypeRequest("ADULT", 21)];
+    expect(() => getNumOfTixAndPrice(requests)).toThrowError(
+      TooManyTicketsException
+    );
   });
-});
-test("1 adult, 1 child => 30 pounds, 2 tickets", () => {
-  const requests = [
-    new TicketTypeRequest("CHILD", 1),
-    new TicketTypeRequest("ADULT", 1),
-  ];
-  expect(getNumOfTixAndPrice(requests)).toEqual({
-    price: 30,
-    numOfTix: 2,
+  test("20 adults, 1 child => throws TooManyTicketsExceptionn", () => {
+    const requests = [
+      new TicketTypeRequest("ADULT", 20),
+      new TicketTypeRequest("CHILD", 1),
+    ];
+    expect(() => getNumOfTixAndPrice(requests)).toThrow(
+      TooManyTicketsException
+    );
   });
-});
-test("2 adult, 3 child => 70 pounds, 5 tickets", () => {
-  const requests = [
-    new TicketTypeRequest("CHILD", 3),
-    new TicketTypeRequest("ADULT", 2),
-  ];
-  expect(getNumOfTixAndPrice(requests)).toEqual({
-    price: 70,
-    numOfTix: 5,
+  test("1 adults, 20 child => throws TooManyTicketsException", () => {
+    const requests = [
+      new TicketTypeRequest("ADULT", 1),
+      new TicketTypeRequest("CHILD", 20),
+    ];
+    expect(() => getNumOfTixAndPrice(requests)).toThrow(
+      TooManyTicketsException
+    );
   });
-});
-test("2 adult, 3 child => 70 pounds, 5 tickets", () => {
-  const requests = [
-    new TicketTypeRequest("CHILD", 3),
-    new TicketTypeRequest("ADULT", 2),
-  ];
-  expect(getNumOfTixAndPrice(requests)).toEqual({
-    price: 70,
-    numOfTix: 5,
+  test("20 adults is fine", () => {
+    const requests = [new TicketTypeRequest("ADULT", 20)];
+    expect(() => getNumOfTixAndPrice(requests)).not.toThrowError();
+  });
+  test("1 adult, 19 child is fine", () => {
+    const requests = [
+      new TicketTypeRequest("ADULT", 1),
+      new TicketTypeRequest("ADULT", 19),
+    ];
+    expect(() => getNumOfTixAndPrice(requests)).not.toThrowError();
   });
 });
